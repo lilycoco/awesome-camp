e093b56901fc8cbd6be47258a240ce4d4614c411
ryoko <ry.sep27@gmail.com>
2022-08-21 22:22:17 +0900

    add about page

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,5 +1,61 @@
-import React from "react";
+import React, { ReactNode } from "react";
+import Title from "../../components/Title";
 
 export default function About() {
-  return <div></div>;
+  const InfoContainer = ({
+    title,
+    description,
+  }: {
+    title: string;
+    description: string | ReactNode;
+  }) => (
+    <div className="flex py-6 border-b border-gray-400 items-start">
+      <div className="w-40 font-bold text-sm pt-2">{title}</div>
+      <div>{description}</div>
+    </div>
+  );
+
+  return (
+    <div className="bg-light-brown-gray">
+      <Title enLabel="About Us" className="m-auto pt-24" />
+      <section className="py-24 max-w-screen-lg m-auto">
+        <div className="mb-36">
+          <div className="font-['Noto_Sans'] text-3xl mb-14">Company</div>
+          <div className="max-w-2xl border-t border-gray-400">
+            <InfoContainer
+              title="Company Name"
+              description="Awesome Camp & Glamping"
+            />
+            <InfoContainer title="Found" description="2014年4月" />
+            <InfoContainer
+              title="Works"
+              description={
+                <ul>
+                  <li>アウトドア用品店（店舗及びネットショップ）</li>
+                  <li>撮影及びイベント現場向けレンタル</li>
+                  <li>アウトドアウェディング及びイベントコーディネート</li>
+                  <li>ウェブメディア運営</li>
+                </ul>
+              }
+            />
+          </div>
+        </div>
+        <div className="mb-20">
+          <div className="font-['Noto_Sans'] text-3xl mb-14">History</div>
+          <div className="max-w-2xl border-t border-gray-400">
+            <InfoContainer
+              title="2014.04"
+              description="「Awesome Barbecue.com」 ウェブメディア開設"
+            />
+            <InfoContainer
+              title="2016.06"
+              description={
+                "「Awesome Camp & Glamping」at BASE ウェブショップ開設、販売・レンタル・コーディネート事業開始"
+              }
+            />
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -1,5 +1,13 @@
 import React from "react";
+import Title from "../../components/Title";
 
 export default function Contact() {
-  return <div></div>;
+  return (
+    <div className="bg-light-brown-gray">
+      <Title enLabel="Contact" className="m-auto pt-24" />
+      <section className="max-w-screen-lg m-auto">
+        <p>お気軽にお問合せください。</p>
+      </section>
+    </div>
+  );
 }

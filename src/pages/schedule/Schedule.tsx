68b9ca04591d34fa2d2e46096ac2a04971a73d42
ryoko <ry.sep27@gmail.com>
2022-08-28 00:37:06 +0900

    fix schedule style

--- src/pages/schedule/Schedule.tsx
+++ src/pages/schedule/Schedule.tsx
@@ -1,29 +1,28 @@
 import React from "react";
 import Balloon from "../../components/Balloon";
 import Title from "../../components/Title";
+import Section from "../../Layouts/Section";
 import { scheduleList } from "../../lib/schedule";
 
 export default function Schedule() {
   return (
-    <div className="bg-light-brown-gray py-24">
+    <Section wrapperClass="bg-light-brown-gray">
       <Title enLabel="Schedule" className="m-auto mb-24" />
-      <section className="m-auto w-fit">
-        <div className="">
-          {scheduleList.map(({ title, description }, index) => (
-            <div className="flex gap-10 relative" key={index}>
-              <div className="absolute border-l-2 border-black h-full left-10" />
-              <div className="font-oleo text-3xl w-20 h-20 flex items-center justify-center rounded-full bg-white border-2 border-black z-10">{`0${
-                index + 1
-              }`}</div>
-              <div key={index} className="mt-5 mb-16 max-w-screen-sm">
-                <h2 className="font-bold text-2xl mb-4">{title}</h2>
-                <div>{description}</div>
-              </div>
+      <div className="m-auto w-fit">
+        {scheduleList.map(({ title, description }, index) => (
+          <div className="relative flex gap-5 md:gap-10" key={index}>
+            <div className="absolute left-8 h-full border-l-2 border-black md:left-10" />
+            <div className="z-10 flex h-16 min-w-[4rem] items-center justify-center rounded-full border-2 border-black bg-white font-oleo text-3xl md:h-20 md:min-w-[5rem]">{`0${
+              index + 1
+            }`}</div>
+            <div key={index} className="mt-3 mb-16 max-w-screen-sm md:mt-5">
+              <h2 className="mb-4 text-2xl font-bold">{title}</h2>
+              <div>{description}</div>
             </div>
-          ))}
-        </div>
-      </section>
+          </div>
+        ))}
+      </div>
       <Balloon />
-    </div>
+    </Section>
   );
 }
